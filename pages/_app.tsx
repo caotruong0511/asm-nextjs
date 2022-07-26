d3b6cf92b4ff61f11afa5b69e7699f7a3da92bd5
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-07-26 16:36:16 +0700

    setup redux-toolkit redux persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.scss";
 import React from "react";
 import { AppPropsWithLayout } from "../models/layout";
-import { ClientLayout, Pro5Layout } from "../layouts";
+import { ClientLayout } from "../layouts";
+import { Provider } from "react-redux";
+import persistor, { store } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -9,13 +12,12 @@ config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <ClientLayout>{page}</ClientLayout>);
-  // const getLayout2 =
-  //   Component.getLayout ??
-  //   ((page) => (
-  //     <ClientLayout>
-  //       <Pro5Layout>{page}</Pro5Layout>
-  //     </ClientLayout>
-  //   ));
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>,
+  );
 }
